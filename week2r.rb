4d8a1f83868516447693d3cee33266dae568f87b
ScStew <scottmstewart2@gmail.com>
2017-06-28 10:41:28 -0400

    second commit

--- week2r.rb
+++ week2r.rb
@@ -1,1 +1,17 @@
-p 1 + rand(9)
+def guess
+	p "guess a number between 1 and 9"
+	x = gets.chomp.to_i
+    y = 1 + rand(9)
+    if 
+    	x == y
+    	p "correct"
+    elsif
+    	x > y
+    	p "too high"
+    else
+    	p "too low"
+    	
+    end
+end
+
+guess
